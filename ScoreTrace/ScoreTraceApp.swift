0ee26956b74491da39d3d7c8a270cd3c86fa77ec
Pablo Penalva <ppenalva@me.com>
2023-02-08 09:53:53 +0100

    Initial Commit

---
+++ ScoreTrace/ScoreTraceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreTraceApp.swift
+//  ScoreTrace
+//
+//  Created by Pablo Penalva on 2/8/23.
+//
+
+import SwiftUI
+
+@main
+struct ScoreTraceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
